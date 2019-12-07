1996fb7cc5eabdc57ef4643f83453d62d9905f8c
Kaique Ewerton Bernardo <devell39@MLTBF.LOCAL>
2019-12-07 16:24:24 -0300

    fim

---
+++ src/components/svg/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const SvgComponent = styled.svg`
+        height:40px;
+        width:40px;
+        transition:  0.3s ease-in-out;
+
+    &:hover {
+       height:70px;
+       width:70px;
+         
+    }`
+
+
+
